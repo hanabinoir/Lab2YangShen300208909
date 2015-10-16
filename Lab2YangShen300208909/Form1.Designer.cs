83b7480198d924320b000c98b61773960dd00c41
hanabinoir <crigshn@gmail.com>
2015-10-16 16:33:31 +0900

    Chukan Shiken

---
+++ Lab2YangShen300208909/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Lab2YangShen300208909
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.phoneTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUD = new System.Windows.Forms.Button();
+            this.btnUL = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(267, 149);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(286, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(12, 168);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 2;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(154, 168);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(100, 20);
+            this.nameTxt.TabIndex = 4;
+            // 
+            // phoneTxt
+            // 
+            this.phoneTxt.Location = new System.Drawing.Point(154, 194);
+            this.phoneTxt.Name = "phoneTxt";
+            this.phoneTxt.Size = new System.Drawing.Size(100, 20);
+            this.phoneTxt.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Phone";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(12, 197);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 7;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUD
+            // 
+            this.btnUD.Location = new System.Drawing.Point(286, 168);
+            this.btnUD.Name = "btnUD";
+            this.btnUD.Size = new System.Drawing.Size(120, 23);
+            this.btnUD.TabIndex = 8;
+            this.btnUD.Text = "Update w/DataGrid";
+            this.btnUD.UseVisualStyleBackColor = true;
+            this.btnUD.Click += new System.EventHandler(this.btnUD_Click);
+            // 
+            // btnUL
+            // 
+            this.btnUL.Location = new System.Drawing.Point(286, 197);
+            this.btnUL.Name = "btnUL";
+            this.btnUL.Size = new System.Drawing.Size(120, 23);
+            this.btnUL.TabIndex = 9;
+            this.btnUL.Text = "Update w/ListView";
+            this.btnUL.UseVisualStyleBackColor = true;
+            this.btnUL.Click += new System.EventHandler(this.btnUL_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(12, 226);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 10;
+            this.btnDel.Text = "Detete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 262);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnUL);
+            this.Controls.Add(this.btnUD);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.phoneTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Lab 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTxt;
+        private System.Windows.Forms.TextBox phoneTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUD;
+        private System.Windows.Forms.Button btnUL;
+        private System.Windows.Forms.Button btnDel;
+    }
+}
+
---
+++ Lab2YangShen300208909/Form1.cs
@@ -0,0 +1,199 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Lab2YangShen300208909
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlTextReader reader = new XmlTextReader("data.xml");
+            string connectionString = "";
+
+            string[] names = { "SERVER", "database", "uid", "password" };
+            string[] values = new string[3];
+            for (int i = 0; i < names.Length; i++)
+            {
+                reader.ReadToFollowing(names[i]);
+                reader.Read();
+                connectionString += names[i] + "=" + reader.Value + ";";
+            }
+
+            reader.Close();
+            connection = new MySqlConnection(connectionString);
+
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("ID");
+            listView1.Columns.Add("Name");
+            listView1.Columns.Add("Phone");
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (connection != null)
+                connection.Close();
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            if (connection != null)
+            {
+                connection.Open();
+                string query = "Select * from Shippers ";
+
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataAdapter mcmd = new MySqlDataAdapter();
+                MySqlDataReader reader;
+
+                mcmd.SelectCommand = cmd;
+                /* mcmd contains the data
+                   mcmd.Fill "transfer" data to DataSet ds */
+                DataSet ds = new DataSet();
+                mcmd.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+
+                reader = cmd.ExecuteReader();
+                string[] data = new string[3];
+                listView1.Clear();
+                listView1.Columns.Add("ID");
+                listView1.Columns.Add("Name");
+                listView1.Columns.Add("Phone");
+                ListViewItem item;
+                while (reader.Read())
+                {
+                    data[0] = reader.GetString(0);
+                    data[1] = reader.GetString(1);
+                    data[2] = reader.GetString(2);
+                    item = new ListViewItem(data);
+                    listView1.Items.Add(item);
+                }
+                reader.Close();
+
+                connection.Close();
+            }
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string instruction = "INSERT INTO Shippers(shippername,phone) ";
+            instruction += " values (@a,@b) ";
+
+            //create command and assign the query and connection from the constructor
+            if (connection != null)
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(instruction, connection);
+
+                cmd.Parameters.AddWithValue("@a", nameTxt.Text);
+                cmd.Parameters.AddWithValue("@b", phoneTxt.Text);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                btnDisplay_Click(sender, e);
+            }
+
+        }
+
+        private void btnUD_Click(object sender, EventArgs e)
+        {
+            int selectedRowCount = dataGridView1.Rows.GetRowCount(DataGridViewElementStates.Selected);
+
+            if (selectedRowCount > 0)
+            {
+                // using DataGridView
+                string instruction = "Update Shippers set shippername = @a, phone = @b where id = @c ";
+
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(instruction, connection);
+
+                cmd.Parameters.AddWithValue("@a", nameTxt.Text);
+                cmd.Parameters.AddWithValue("@b", phoneTxt.Text);
+                cmd.Parameters.Add("@c", MySqlDbType.String);
+                for (int y = 0; y < selectedRowCount; y++)
+                {
+                    // cell[0] is the id
+                    cmd.Parameters["@c"].Value = dataGridView1.SelectedRows[y].Cells[0].Value.ToString();
+                    cmd.ExecuteNonQuery();
+                }
+
+                connection.Close();
+                btnDisplay_Click(sender, e);
+            }            
+
+        }
+
+        private void btnUL_Click(object sender, EventArgs e)
+        {
+            //using ListView
+            ListView.SelectedIndexCollection indexes = listView1.SelectedIndices;
+            if (indexes.Count > 0)
+            {
+                string instruction = "Update Shippers set shippername = @a, phone = @b where id = @c ";
+
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(instruction, connection);
+
+                cmd.Parameters.AddWithValue("@a", nameTxt.Text);
+                cmd.Parameters.AddWithValue("@b", phoneTxt.Text);
+                cmd.Parameters.Add("@c", MySqlDbType.String);
+                foreach (int y in indexes)
+                {
+                    // cell[0] is the id
+                    cmd.Parameters["@c"].Value = listView1.Items[y].SubItems[0].Text;
+                    cmd.ExecuteNonQuery();
+                }
+
+                connection.Close();
+                btnDisplay_Click(sender, e);
+            }         
+
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            int selectedRowCount = dataGridView1.Rows.GetRowCount(DataGridViewElementStates.Selected);
+
+            if (selectedRowCount > 0)
+            {
+                // using DataGridView
+                string instruction = "Delete from Shippers where id = @c ";
+
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(instruction, connection);
+
+                cmd.Parameters.Add("@c", MySqlDbType.String);
+                for (int y = 0; y < selectedRowCount; y++)
+                {
+                    // cell[0] is the id
+                    cmd.Parameters["@c"].Value = dataGridView1.SelectedRows[y].Cells[0].Value.ToString();
+                    cmd.ExecuteNonQuery();
+                }
+
+                connection.Close();
+                btnDisplay_Click(sender, e);
+            }          
+
+        }
+    }
+}
